0a69c59c4c6b7783846e1c56b2f6211763b89067
Roma <romassingh09@gmail.com>
2025-10-02 19:33:25 +0530

    First Commit

---
+++ Listing/ListingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListingApp.swift
+//  Listing
+//
+//  Created by roma singh on 02/10/25.
+//
+
+import SwiftUI
+
+@main
+struct ListingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
